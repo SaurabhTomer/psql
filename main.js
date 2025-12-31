4dbcb45674e648c5c1d948df4fe8f4f7a6a033ef
Saurabh Tomer <tomersaurabh91@gmail.com>
2025-12-31 22:25:32 +0530

    PSQL SETUP

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
-import  { Client } from 'pg'
 
+import { Client } from "pg";
 
-const connection = new Client({
-    host:"localhost",
-    user:"postgres",
-    port:5432,
-})
+const pgClient = new Client({
+  user: "neondb_owner",
+  password: "npg_obljQip8vO5y",
+  port: 5432,
+  host: "ep-dark-sky-adkk4hju-pooler.c-2.us-east-1.aws.neon.tech",
+  database: "neondb",
+   ssl: {
+    require: true,
+    rejectUnauthorized: false,
+  },
+});
+
+async function main(){
+    console.log('hello')
+    await pgClient.connect();
+
+    const response = await pgClient.query(" UPDATE users SET username = 'SAURABH' WHERE ID='1' ");
+    console.log(response.rows);
+}
+
+main();
